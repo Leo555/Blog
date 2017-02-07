e584d775a966825204ce0b8042f8117cf8f1a8e6
LeoLi <leo.z.li@oocl.com>
2017-02-07 17:25:21 +0800

    Revert "update"
    This reverts commit 9e2696315557de4b2512217fee0c3ff5175f3886.

---
+++ themes/yilia/webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require("webpack");
+var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: "./source-src/js/main.js"
+  },
+  output: {
+    path: "./source",
+    publicPath: "/",
+    filename: "[name].js"
+  },
+  module: {
+    loaders: [{
+      test: /.scss$/,
+      loader: ExtractTextPlugin.extract('style', 'css!sass!postcss-loader')
+    }, {
+      test: /\.(gif|jpg|png)\??.*$/,
+      loader: 'url-loader?limit=5000&name=img/[name].[ext]'
+    }, {
+      test: /\.(woff|svg|eot|ttf)\??.*$/,
+      loader: "file-loader?name=fonts/[name].[ext]"
+    }]
+  },
+  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+  watch: true
+}
