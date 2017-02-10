df4ecb76c9ecbcc26f7ee6052e573a1f29cbf3ad
LeoLi <leo.z.li@oocl.com>
2017-02-10 10:01:39 +0800

    update

--- themes/yilia/source-src/js/main.js
+++ themes/yilia/source-src/js/main.js
@@ -1,36 +1,15 @@
-require('../css/main.scss')
+// 样式
+import '../css/main.scss'
+// 上报
+import './report'
+// 图片查看器
+import Viewer from './viewer'
+// 分享
+import Share from './share'
 
-require('./jquery')
+import {addLoadEvent} from './util'
 
-var tags = require('./tags')
-var archiveInner = require('./archive-inner')
-var tools = require('./tools')
-var browser = require('./browser')
-var fixPage = require('./fix-page')
-var mobile = require('./mobile')
-var viewer = require('./viewer')
-require('./jquery.lazyload')
-
-$(function() {
-	viewer.init()
-	archiveInner.init()
-	fixPage.init()
-	tags.init()
-	// todo: resize destrop
-	if(browser.versions.mobile === true && $(window).width() < 800){
-		mobile.init()
-	}else{
-		tools.init()
-		$('.js-smart-menu').click(function(e) {
-			e.stopPropagation()
-			tools.show($(this).data('idx'))
-		})
-		$('.left-col,.mid-col').click(function() {
-			tools.hide()
-		})
-	}
-
-	if (yiliaConfig && yiliaConfig.open_in_new) {
-		$('.article-entry a').not('.article-more-a').attr('target', '_blank')
-	}
+addLoadEvent(function() {
+	Share.init()
+	Viewer.init()
 })
